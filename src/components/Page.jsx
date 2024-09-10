08b2e1e9af8bd492ea4f1c261f5373ba2e3e92ab
Rasi Kumar <rasimax55@gmail.com>
2024-09-10 18:35:14 +0530

    hidden h1

---
+++ src/components/Page.jsx
@@ -0,0 +1,17 @@
+// import { Helmet } from 'react-helmet';
+// import Heart from './Heart'
+
+// const Page = () => {
+//   const canonicalUrl = 'https://evvisolutions.com/'; // Update this as needed
+
+//   return (
+//     <div>
+//       <Helmet>
+//         <link rel="canonical" href={canonicalUrl} />
+//       </Helmet>
+//       <Heart/>
+//     </div>
+//   );
+// };
+
+// export default Page;
